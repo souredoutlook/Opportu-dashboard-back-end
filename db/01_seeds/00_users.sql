7a200a3df61a5b9472943c90635dce46f70e00c3
souredoutlook <souredoutlook@gmail.com>
2021-05-08 11:16:33 -0400

    add admin user to seeds for test

--- db/01_seeds/00_users.sql
+++ db/01_seeds/00_users.sql
@@ -1,3 +1,5 @@
 --1
-INSERT INTO users (first_name, last_name, email, password)
-VALUES ('Nick', 'Meisenheimer', 'nicholas@meisen.haus', '$2b$10$dZzQQWiYPHW6cBnsG2bG3OcAbn9OALv16IfcYiiDC/GnphwiDlSQO');
+INSERT INTO users (first_name, last_name, email, password, is_admin)
+VALUES
+  ('Nick', 'Meisenheimer', 'nicholas@meisen.haus', '$2b$10$dZzQQWiYPHW6cBnsG2bG3OcAbn9OALv16IfcYiiDC/GnphwiDlSQO', false),
+  ('Nick', 'Meisenheimer', 'nicholas@opportu.com', '$2b$10$dZzQQWiYPHW6cBnsG2bG3OcAbn9OALv16IfcYiiDC/GnphwiDlSQO', true);
