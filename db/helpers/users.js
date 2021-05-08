4a3ac16ef1cf7f996144cccc995f7f096a29a3ff
souredoutlook <souredoutlook@gmail.com>
2021-05-08 11:18:07 -0400

    initialize getUserById, addUser queries

--- db/helpers/users.js
+++ db/helpers/users.js
@@ -45,4 +45,50 @@ const getUserByEmail = function(email) {
   })
  }
 
-module.exports = { getUsers, getUserByEmail }
+ /**
+ * Returns user with matching id
+ * @param {number} id 
+ * @returns an object representing user with id that equals given argument
+ */
+const getUserById = function(id) {
+  const queryParams = [id];
+  const queryString = `
+  SELECT *
+  FROM users
+  WHERE id = $1;
+  `;
+
+  return db.query(queryString, queryParams)
+  .then((res) => {
+    if (res.rows.length > 0) {
+      //validations should ensure that only one result is possible
+      return res.rows[0];
+    } else {
+      //return null when no user is found
+      return null;
+    }
+  })
+ }
+
+ /**
+  * Insert a user with valid information into the database.
+  * @param {string} first_name 
+  * @param {string} last_name 
+  * @param {string} email 
+  * @param {string} password 
+  */
+ const addUser = function(first_name, last_name, email, password) {
+   const queryParams = [first_name, last_name, email, password];
+   const queryString = `
+    INSERT INTO users (first_name, last_name, email, password)
+    VALUES ($1, $2, $3, $4)
+    RETURNING *;
+   `;
+
+   return db.query(queryString, queryParams)
+    .then(res => {
+      return res.rows[0];
+    })
+ }
+
+module.exports = { getUsers, getUserByEmail, getUserById, addUser }
