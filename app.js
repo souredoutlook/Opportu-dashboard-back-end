cf6f44b44824edce01205d75c6740fb515b0c0c0
souredoutlook <souredoutlook@gmail.com>
2021-05-07 22:29:39 -0400

    add cookie-session as a dependency

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 const db = require("./db/helpers/index");
+const bcrypt = require('bcrypt');
+
+require('dotenv').config();
+
+const {
+  KEY1,
+  KEY2,
+} = process.env;
+
 
 const express = require('express');
 const logger = require('morgan');
-const bcrypt = require('bcrypt');
+const cookieSession = require('cookie-session');
+
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const sessionsRouter = require('./routes/sessions');
 
 const app = express();
+
+app.use(cookieSession({
+  name: 'session',
+  keys: [KEY1, KEY2],
+
+  // Cookie Options
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}));
 
 app.use(logger('dev'));
 app.use(express.json());
