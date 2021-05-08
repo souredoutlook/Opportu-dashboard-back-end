bf23518fa82f3a53f5c592883e8277dfa6e89a8d
souredoutlook <souredoutlook@gmail.com>
2021-05-08 11:18:30 -0400

    initialize addUserIfUnique aggregate query

--- db/helpers/index.js
+++ db/helpers/index.js
@@ -1,6 +1,6 @@
 //index.js -> all of the data helpers flow through this file
 
-const { getUsers, getUserByEmail } = require("./users")
+const { getUsers, getUserByEmail, getUserById, addUser } = require("./users")
 
 /**
  * Compares password to hashed password in db for user with a given email
@@ -10,20 +10,54 @@ const { getUsers, getUserByEmail } = require("./users")
  */
 const validateUser = function(email, password, bcrypt) {
   return getUserByEmail(email)
-    .then(row => {
-
-      if (row && bcrypt.compareSync(password, row.password)) {
-        return row.id
+    .then(res => {
+      if (res && bcrypt.compareSync(password, res.password)) {
+        return res.id
       } else {
         return null;
       }
-
     });
 
 };
 exports.validateUser = validateUser;
 
+const isAdmin = function(id) {
+  return getUserById(id)
+    .then(res => {
+      if(res) {
+        if (res.is_admin === true) {
+          return true
+        } else {
+          return false
+        }
+      } else {
+        return null
+      }
+    });
+};
+exports.isAdmin = isAdmin;
+
+/**
+ * Validates uniqueness of email in db and then calls addUser if valid
+ * @param {*} first_name 
+ * @param {*} last_name 
+ * @param {*} email 
+ * @param {*} password 
+ * @returns addUser query promise
+ */
+const addUserIfUnique = function(first_name, last_name, email, password, bcrypt) {
+
+  return getUserByEmail(email)
+    .then(res => {
+      if (res) {
+        return null
+      } else {
+        const encryptedPassword = bcrypt.hashSync(password, 10);
+        return addUser(first_name, last_name, email, encryptedPassword);
+      }
+    })
+};
+exports.addUserIfUnique = addUserIfUnique;
+
 // --- users ---
 exports.getUsers = getUsers;
-
-// $2b$10$dZzQQWiYPHW6cBnsG2bG3OcAbn9OALv16IfcYiiDC/GnphwiDlSQO
