4057c0f425cda117aa6bdd6617b8c4274e7098f1
souredoutlook <souredoutlook@gmail.com>
2021-05-10 14:39:48 -0400

    add WHERE clause to core_values example query

--- db/02_queries/01_values_assessment_details.sql
+++ db/02_queries/01_values_assessment_details.sql
@@ -3,4 +3,5 @@ SELECT user_id, assessment_id, core_value, name as custom_value, rank
 FROM values_assessments
 JOIN ranked_values ON values_assessments.id = assessment_id
 FULL OUTER JOIN custom_values ON custom_value_id = custom_values.id
+WHERE user_id = 1
 ORDER BY assessment_id, rank;
