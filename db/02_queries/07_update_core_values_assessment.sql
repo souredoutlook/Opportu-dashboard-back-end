03d58adc81861a220e51e99afbe7b3ebf06dca8f
souredoutlook <souredoutlook@gmail.com>
2021-05-11 11:25:03 -0400

    sample core_values insertion query

---
+++ db/02_queries/07_update_core_values_assessment.sql
@@ -0,0 +1,22 @@
+INSERT INTO ranked_values (assessment_id, core_value, custom_value_id, rank)
+VALUES
+  --1
+  (1, 'Appreciation', NULL, 1),
+  --2
+  (1, 'Attractiveness', NULL, 2),
+  --3
+  (1, 'Autonomy', NULL, 3),
+  --4
+  (1, 'Balance', NULL, 4),
+  --5
+  (1, 'Being the Best', NULL, 5),
+  --6
+  (1, 'Benevolence', NULL, 6),
+  --7
+  (1, 'Boldness', NULL, 7),
+  --8
+  (1, 'Brilliance', NULL, 8),
+  --9
+  (1, 'Calmness', NULL, 9),
+  --10
+  (1, 'Caring', NULL, 10);
