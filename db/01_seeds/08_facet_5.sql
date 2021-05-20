1b2b754a053e30362e7a82f76ff60a3c2a824164
souredoutlook <souredoutlook@gmail.com>
2021-05-20 14:59:28 -0400

    update facet_5 seed for test

--- db/01_seeds/08_facet_5.sql
+++ db/01_seeds/08_facet_5.sql
@@ -1,2 +1,5 @@
+INSERT INTO facet_5_assessments (user_id, will, energy, control, emotionality, affection, completed)
+VALUES (1, 5, 6, 7, 8, 9, NOW()::TIMESTAMP);
+
 INSERT INTO facet_5_assessments (user_id)
-VALUES (1);
+VALUES (2);
