92674e44f68cde92388be35fb25d7fad15cdd7eb
souredoutlook <souredoutlook@gmail.com>
2021-05-20 22:48:34 -0400

    refactor facet_5 query for test

--- db/01_seeds/08_facet_5.sql
+++ db/01_seeds/08_facet_5.sql
@@ -1,5 +1,2 @@
 INSERT INTO facet_5_assessments (user_id, will, energy, control, emotionality, affection, completed)
-VALUES (1, 5, 6, 7, 8, 9, NOW()::TIMESTAMP);
-
-INSERT INTO facet_5_assessments (user_id)
-VALUES (2);
+VALUES (2, 5, 6, 7, 8, 9, NOW()::TIMESTAMP);
