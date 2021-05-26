eaa6fe755cb54a2388d45bedefb1760a719e6bea
souredoutlook <souredoutlook@gmail.com>
2021-05-25 22:50:28 -0400

    initialize groups helper

---
+++ db/helpers/groups.js
@@ -0,0 +1,34 @@
+const db = require("../pool")
+
+ /**
+  * Insert a group with valid info into the database if unique
+  * @param {string} name
+  * @param {string} description
+  * @returns {object} returns a new user object
+  */
+ const addGroupIfUnique = function(name, description) {
+   const queryParams = [name, description, name];
+   const queryString = `
+    INSERT INTO groups (name, description)
+    SELECT $1, $2
+    WHERE NOT EXISTS (
+      SELECT 1 FROM groups WHERE name = $3
+    )
+    RETURNING *;
+   `;
+
+   return db.query(queryString, queryParams)
+    .then(res => {
+      if (res.rows) {
+        return res.rows[0];
+      } else {
+        //not unique
+        return null;
+      }
+    })
+    .catch(err => {
+      return null
+    })
+ }
+
+ module.exports = { addGroupIfUnique };
