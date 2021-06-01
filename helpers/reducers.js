54df4442bdc2994e455f2c8b1aef60229450a843
souredoutlook <souredoutlook@gmail.com>
2021-06-01 10:20:38 -0400

    filter values assessments with agg_id

--- helpers/reducers.js
+++ helpers/reducers.js
@@ -2,12 +2,14 @@
 
 const core_values_reducer = function(rows) {
   return rows.reduce((prev, cur)=> {
-    const { assessment_id, core_value, custom_value, completed } = cur;
-
-    if (prev[assessment_id] === undefined) {
-      prev[assessment_id] = {values: [core_value || custom_value], completed };
-    } else {
-      prev[assessment_id].values.push(core_value || custom_value);
+    const { assessment_id, core_value, custom_value, completed, aggregate_assessment_id } = cur;
+    
+    if (!aggregate_assessment_id) {
+      if (prev[assessment_id] === undefined) {
+        prev[assessment_id] = {values: [core_value || custom_value], completed };
+      } else {
+        prev[assessment_id].values.push(core_value || custom_value);
+      }
     }
 
     return prev;
