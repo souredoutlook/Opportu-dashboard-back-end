db14f5adeea5a48afda1fa295a40a638d71fcdd7
souredoutlook <souredoutlook@gmail.com>
2021-06-01 10:00:01 -0400

    adjust values assessments to reference aggregate

--- db/00_schema/06_1_values_assessments.sql
+++ db/00_schema/06_1_values_assessments.sql
@@ -3,6 +3,7 @@ DROP TABLE IF EXISTS values_assessments CASCADE;
 CREATE TABLE values_assessments (
   id SERIAL PRIMARY KEY,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  aggregate_assessment_id INTEGER REFERENCES aggregate_assessments(id) DEFAULT NULL,
   created TIMESTAMP NOT NULL DEFAULT NOW()::TIMESTAMP,
   completed TIMESTAMP --insert on user completion
 );
