c3a0473308959c53908d9de21dceca959c26ab0c
souredoutlook <souredoutlook@gmail.com>
2021-06-01 10:02:54 -0400

    add POST /groups/values to groups router

--- routes/groups.js
+++ routes/groups.js
@@ -3,6 +3,43 @@ const router = express.Router();
 
 module.exports = (db) => {
 
+  router.post('/values', function(req, res) {
+    const userId = req.session && req.session.userId;
+    const id = req.body.groupId;
+    
+    //check session
+    if (userId) {
+      //check if user is admin
+      db.isAdmin(userId)
+      .then(isAdmin => {
+        if (isAdmin) {
+         // if they are admin
+         if (id) {
+          //assign an assignment to each user associated with the groupid
+          db.assignAggregateCoreValues(id, null)
+            .then(response => {
+              if (response) {
+                res.send(response).status(200);
+              } else {
+                res.sendStatus(400);
+              }
+            })
+            .catch(err => res.sendStatus(400));
+         } else {
+           // no id provided
+           res.sendStatus(400);
+         }
+        } else {
+          //not admin
+          res.sendStatus(403);
+        }
+      });
+    } else {
+      //no session
+      res.sendStatus(401);
+    }
+  });
+
   router.post('/', function(req, res) {
     const userId = req.session && req.session.userId;
     const { name, description } = req.body;
