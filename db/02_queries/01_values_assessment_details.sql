3f06632fecdb3b436727545202e036f0f4a23caa
souredoutlook <souredoutlook@gmail.com>
2021-06-01 10:19:26 -0400

    adjust sample query for values assessment details

--- db/02_queries/01_values_assessment_details.sql
+++ db/02_queries/01_values_assessment_details.sql
@@ -1,7 +1,7 @@
 --details of each values_assessment
-SELECT completed, user_id, assessment_id, core_value, name as custom_value, rank
+SELECT completed, user_id, assessment_id, core_value, name as custom_value, rank, aggregate_assessment_id
 FROM values_assessments
 JOIN ranked_values ON values_assessments.id = assessment_id
 FULL OUTER JOIN custom_values ON custom_value_id = custom_values.id
-WHERE user_id = 2
+WHERE user_id = 1
 ORDER BY completed DESC, assessment_id, rank;
