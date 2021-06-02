d9dff78f2526e9e082d1de17c2131530c3a89bb1
souredoutlook <souredoutlook@gmail.com>
2021-06-01 22:29:24 -0400

    add getAgg..ResultsById to agg..assessments helpers

--- db/helpers/aggregate_assessments.js
+++ db/helpers/aggregate_assessments.js
@@ -1,5 +1,7 @@
 const db = require('../pool');
 
+const { aggregate_assessment_reducer } = require('../../helpers/reducers');
+
 /**
  * Creates a new aggregate_assessment that references either a group_id or a team_id
  * @param {number} groupId or null
@@ -27,4 +29,28 @@ const db = require('../pool');
      //return null when the groupId or teamId is an invalid type
 };
 
-module.exports = { addAggregateAssessmentById };
+const getAggregateAssessmentResultsById = function(id) {
+  const queryParams = [id]
+  const queryString = `
+    SELECT core_value, custom_values.name AS custom_value
+    FROM aggregate_assessments
+    LEFT JOIN values_assessments ON values_assessments.aggregate_assessment_id = aggregate_assessments.id
+    LEFT JOIN ranked_values ON values_assessments.id = assessment_id
+    LEFT JOIN custom_values ON custom_value_id = custom_values.id
+    WHERE values_assessments.completed IS NOT NULL AND aggregate_assessment_id = $1;
+  `;
+
+  return db.query(queryString, queryParams)
+     .then(res => {
+       if (res.rows && res.rows.length > 0) {
+        return aggregate_assessment_reducer(res.rows);
+      } else {
+        //return null when the aggregate_assessment_id does not exist
+        return null;
+      }
+     })
+     .catch(err => null);
+     //return null when the id's type is invalid
+}
+
+module.exports = { addAggregateAssessmentById, getAggregateAssessmentResultsById };
