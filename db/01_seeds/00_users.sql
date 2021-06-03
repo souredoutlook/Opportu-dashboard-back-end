2618a34e34619a17bf19526c5273bfc1c32e302a
souredoutlook <souredoutlook@gmail.com>
2021-06-02 21:38:09 -0400

    adjust usesrs seeds to reflect functional emails

--- db/01_seeds/00_users.sql
+++ db/01_seeds/00_users.sql
@@ -2,4 +2,4 @@
 INSERT INTO users (first_name, last_name, email, password, is_admin)
 VALUES
   ('Nick', 'Meisenhaus', 'nicholas@meisen.haus', '$2b$10$dZzQQWiYPHW6cBnsG2bG3OcAbn9OALv16IfcYiiDC/GnphwiDlSQO', false),
-  ('Nick', 'Meisenheimer', 'nicholas@opportu.com', '$2b$10$dZzQQWiYPHW6cBnsG2bG3OcAbn9OALv16IfcYiiDC/GnphwiDlSQO', true);
+  ('Nick', 'Meisenheimer', 'nicholas.meisenheimer@opportu.com', '$2b$10$dZzQQWiYPHW6cBnsG2bG3OcAbn9OALv16IfcYiiDC/GnphwiDlSQO', true);
