201cfe2cbd99af17d5e3e72bb1d7da910a43b6d3
souredoutlook <souredoutlook@gmail.com>
2021-06-03 11:07:50 -0400

    add assessments mailer to POST /assessments/values

--- routes/assessments.js
+++ routes/assessments.js
@@ -1,13 +1,7 @@
-require('dotenv').config();
-const {
-  MAILER_USER,
-  FRONT_END_PATH,
-} = process.env;
-
 const express = require('express');
 const router = express.Router();
 const { validateRankedValues, validateFacets } = require('../helpers/validations');
-
+const mailer = require('../mailers/assessments');
 module.exports = (db, transporter) => {
 
   router.post('/values', function(req, res) {
@@ -29,40 +23,7 @@ module.exports = (db, transporter) => {
                 .then(user => {
                   if (user) {
                     res.send(core_values_assessment).status(200);
-                    
-                    const {first_name, last_name, email} = user;
-                    const assessment_id = core_values_assessment.id;
-                    
-                    const mailOptions = {
-                      from: MAILER_USER,
-                      to:  MAILER_USER,
-                      subject: `Root Values Assessment for ${first_name} ${last_name}`,
-                      text:`Assessment Details: \nName - ${first_name} ${last_name} \nUser Email - ${email} \nAssessment Link - ${FRONT_END_PATH}/assessments/${assessment_id}`
-                    };
-                    
-                    transporter.sendMail(mailOptions, function(error, info){
-                      if (error) {
-                        console.log(error);
-                        const mailOptions = {
-                          from: MAILER_USER,
-                          to: MAILER_NOTICE,
-                          subject: `Failed to send new user details for ${first_name} ${last_name}`,
-                          text: `Unable to deliver user details for ${first_name} ${last_name} please try again.`
-                        };
-    
-                        transporter.sendMail(mailOptions, function(error, info) {
-                          if (error) {
-                            console.log('Failed to send failure notice: ', error);
-                          } else {
-                            console.log(console.log('Email sent: ' + info.response));
-                          }
-                        });
-    
-                      } else {
-                        console.log('Email sent: ' + info.response);
-                      }
-                    });
-
+                    mailer(transporter, user, core_values_assessment, adminEmail);
                   } else {
                     res.sendStatus(400);
                   }
