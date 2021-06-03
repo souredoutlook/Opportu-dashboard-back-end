08429c73da1977b0f2fad2d877c8c158ca72ed5f
souredoutlook <souredoutlook@gmail.com>
2021-06-03 11:00:33 -0400

    add users mailer to POST /users

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,7 @@
-require('dotenv').config();
-const {
-  MAILER_USER,
-  MAILER_NOTICE,
-} = process.env;
-
 const express = require('express');
 const router = express.Router();
 const { validateEmail, validatePassword } = require('../helpers/validations');
+const mailer = require('../mailers/users');
 
 module.exports = (db, bcrypt, transporter) => {
 
@@ -24,37 +19,7 @@ module.exports = (db, bcrypt, transporter) => {
             .then(newUser => {
               if (newUser) {
                 res.send(newUser).status(200);
-
-                const mailOptions = {
-                  from: MAILER_USER,
-                  to:  adminEmail,
-                  subject: `Successfully added user ${first_name} ${last_name}`,
-                  text: `User details: \nName - ${first_name} ${last_name} \nEmail - ${email} \nPassword - ${password}`
-                };
-
-                transporter.sendMail(mailOptions, function(error, info) {
-                  if (error) {
-                    console.log(error);
-                    const mailOptions = {
-                      from: MAILER_USER,
-                      to: MAILER_NOTICE,
-                      subject: `Failed to send new user details for ${first_name} ${last_name}`,
-                      text: `Unable to deliver user details for ${first_name} ${last_name} please try again.`
-                    };
-
-                    transporter.sendMail(mailOptions, function(error, info) {
-                      if (error) {
-                        console.log('Failed to send failure notice: ', error);
-                      } else {
-                        console.log(console.log('Email sent: ' + info.response));
-                      }
-                    });
-
-                  } else {
-                    console.log('Email sent: ' + info.response);
-                  }
-                });
-
+                mailer(transporter, first_name, last_name, email, password, adminEmail);
               } else {
                 //email is valid but not unique
                 res.sendStatus(400);
